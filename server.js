8a99c192cab94617753553f75fcdfef952a1a2f9
Ruttasate <m18260.29@gmail.com>
2023-02-05 07:38:23 +0900

    finish ass#5

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const hospitals = require("./routes/hospitals");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
 
+connectDB();
+
 const app = express();
+app.use(express.json());
 app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     "Server running in ",
@@ -17,3 +21,8 @@ app.listen(
     PORT
   )
 );
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
