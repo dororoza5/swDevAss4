d8a1ebecf705a64d6a88d1bd278174e0834424f0
Ruttasate <m18260.29@gmail.com>
2023-02-22 16:40:07 +0900

    finish ass#6

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -8,12 +8,16 @@ const {
 } = require("../controllers/hospitals");
 
 const router = express.Router();
+const { protect, authorize } = require("../middleware/auth");
 
-router.route("/").get(getHospitals).post(createHospital);
+router
+  .route("/")
+  .get(getHospitals)
+  .post(protect, authorize("admin"), createHospital);
 router
   .route("/:id")
   .get(getHospital)
-  .put(updateHospital)
-  .delete(deleteHospital);
+  .put(protect, authorize("admin"), updateHospital)
+  .delete(protect, authorize("admin"), deleteHospital);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+
 const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
 const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
@@ -9,7 +12,9 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
